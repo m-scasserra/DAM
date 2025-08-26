c4257068e158b3c4aae939afb21e0560588f7d64
Marco Scasserra <marco.scasserra@gmail.com>
2025-08-26 02:02:05 -0300

    Refactor DispositivoService to improve error handling and response structure for fetching dispositivos

--- app-dam/src/frontend/dam/src/app/services/dispositivo.service.ts
+++ app-dam/src/frontend/dam/src/app/services/dispositivo.service.ts
@@ -9,7 +9,42 @@ export class DispositivoService {
 
   constructor(private _http: HttpClient) { }
 
-  getDispositivos () {
-    return firstValueFrom(this._http.get("http://localhost:8000/dispositivos"))
+  async getAllDispositivos(): Promise<Dispositivo[]> {
+    try {
+      const response = await firstValueFrom(
+        this._http.get<{success: boolean; data: Dispositivo[]; count: number}>("http://localhost:8000/api/dispositivos")
+      );
+      if (response.success) {
+        return response.data;
+      } else {
+        throw new Error("No se pudieron cargar los dispositivos");
+      }
+    } catch (error) {
+      console.error("Error fetching dispositivos", error);
+      throw new Error("No se pudieron cargar los dispositivos");
+    }
   }
+
+  async getDispositivo(id: number): Promise<Dispositivo | null> {
+    try {
+      const response = await firstValueFrom(
+        this._http.get<{success: boolean; data: Dispositivo; count: number}>("http://localhost:8000/api/dispositivo/" + id)
+      );
+      if (response.success) {
+        return response.data;
+      } else {
+        throw new Error("No se pudo cargar el dispositivo");
+      }
+    } catch (error) {
+      console.error("Error fetching dispositivo", error);
+      throw new Error("No se pudo cargar el dispositivo");
+    }
+  }
+}
+
+export interface Dispositivo {
+  dispositivoId: number;
+  electrovalvulaId: number;
+  nombre: string;
+  ubicacion: string;
 }
