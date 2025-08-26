59abae51ac1d40d16fe73bc0a712a7b1344fb0b4
Marco Scasserra <marco.scasserra@gmail.com>
2025-08-26 01:58:55 -0300

    Added backend routers for dispositivos, electrovalvulas and mediciones

--- app-dam/src/backend/index.js
+++ app-dam/src/backend/index.js
@@ -6,6 +6,8 @@ var cors = require('cors')
 const jwt = require('jsonwebtoken')
 
 const routerDispositivos = require('./dispositivos/index')
+const routerMediciones = require('./mediciones/index')
+const routerElValvulas = require('./electrovalvulas/index')
 
 var app = express();
 
@@ -14,15 +16,18 @@ const corsOptions = {
     origin: '*',
 }
 
+
 const YOUR_SECRET_KEY = 'mi llave'
 var testUser = {username: 'test', password: '1234'}
+
+app.use(cors(corsOptions))
 
 // to parse application/json
 app.use(express.json()); 
 // to serve static files
 app.use(express.static('/home/node/app/static/'));
 
-app.use(cors(corsOptions))
+
 
 //=======[ Main module code ]==================================================
 
@@ -41,21 +46,6 @@ var authenticator = function (req, res, next) {
     next()
 }
 
-
-var cb0 = function (req, res, next) {
-    console.log('CB0')
-    
-    next()
-}
-
-var cb1 = function (req, res, next) {
-    console.log('CB1')
-    next()
-}
-
-var cb2 = function (req, res, next) {
-    res.send({'mensaje': 'Hola DAM!'}).status(200)
-}
 
 app.post('/login', (req, res) => {
     if (req.body) {
@@ -79,18 +69,9 @@ app.post('/login', (req, res) => {
     }
 })
 
-app.get('/prueba', authenticator, function(req, res) {
-    res.send({message: 'Está autenticado, accede a los datos'})
-})
-
-app.all('/secreto', function (req, res, next) {
-    console.log(req.method)
-    res.send('Secreto').status(200)
-})
-
-app.get('/', [cb0, cb1, cb2]);
-
 app.use(authenticator, routerDispositivos)
+app.use(authenticator, routerMediciones)
+app.use(authenticator, routerElValvulas)
 
 app.listen(PORT, function(req, res) {
     console.log("NodeJS API running correctly");
